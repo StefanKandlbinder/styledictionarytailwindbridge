f001254de08d964a6ecc9693c4c46873056f1837
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2021-12-03 22:35:42 +0100

    shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const spacing = require("./src/assets/styles/spacing.tokens.tailwind.js")
 const fontFamily = require("./src/assets/styles/font.family.tokens.tailwind.js")
 const fontWeight = require("./src/assets/styles/font.weight.tokens.tailwind.js")
 const fontSize = require("./src/assets/styles/font.size.tokens.tailwind.js")
+const boxShadow = require("./src/assets/styles/shadow.tokens.tailwind.js")
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
@@ -119,16 +120,7 @@ module.exports = {
       4: '4px',
       8: '8px',
     },
-    boxShadow: {
-      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-      none: 'none',
-    },
+    boxShadow,
     caretColor: (theme) => theme('colors'),
     contrast: {
       0: '0',
