4429f7032d64fa6343d14579814dff7cde399fa2
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2022-01-03 16:01:59 +0100

    toc

--- src/app/toc/toc.stories.ts
+++ src/app/toc/toc.stories.ts
@@ -8,7 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 @Component({
   selector: 'stw-toc-example',
   template: `
-    <div class="grid grid-cols-9" #tocContainer>
+    <div class="grid grid-cols-9 gap-4" #tocContainer>
       <div class="col-span-7">
         <h1 data-toc-title>Title</h1>
         <h2 id="header" data-toc class="pt-4 mb-3 z-10">Header</h2>
@@ -31,7 +31,7 @@ import { RouterModule, Routes } from '@angular/router';
         <p>Vel orci porta non pulvinar. Placerat vestibulum lectus mauris ultrices eros in. Lectus sit amet est placerat in egestas erat imperdiet. Adipiscing elit ut aliquam purus. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Egestas dui id ornare arcu odio ut. Suspendisse ultrices gravida dictum fusce ut placerat. Volutpat consequat mauris nunc congue nisi.</p>
         <p>Ac felis donec et odio pellentesque diam volutpat commodo sed. Amet tellus cras adipiscing enim eu turpis egestas. Vulputate mi sit amet mauris commodo quis imperdiet. Gravida arcu ac tortor dignissim convallis aenean et. Laoreet sit amet cursus sit amet dictum sit amet justo. Tellus integer feugiat scelerisque varius morbi enim nunc. Convallis a cras semper auctor neque vitae tempus quam. Facilisi nullam vehicula ipsum a arcu cursus vitae congue. Vitae aliquet nec ullamcorper sit amet risus nullam eget.</p>
       </div>
-      <aside class="col-span-2">
+      <aside class="col-span-2 bg-gray-100 p-2 rounded-xl">
         <div class="sticky top-2">
           <stw-toc #toc></stw-toc>
         </div>
